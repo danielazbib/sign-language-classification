7cd129a97b0dd091ebe15a8eef7cb7d46c03a3ae
danielazbib <danielazbib@gmail.com>
2023-12-09 23:56:55 -0500

    trained and evaluated best ml classification model

--- model_training.py
+++ model_training.py
@@ -1,23 +1,43 @@
 import pickle
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 import numpy as np
 
 data_dict = pickle.load(open('data.pickle', 'rb'))
 
 max_len = max(len(entry) for entry in data_dict['data'])
 
+#preprocessing data due to inhomogenous shape of handmark data points
 data_padded = [entry + [0] * (max_len - len(entry)) for entry in data_dict['data']]
-data = np.asarray(data_padded)
 
+data = np.asarray(data_padded)
 labels = np.asarray(data_dict['labels'])
 
 x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.3, shuffle=True, stratify=labels)
-model = RandomForestClassifier()
 
-model.fit(x_train, y_train)
-y_predict = model.predict(x_test)
+classifiers = {
+    'Naive Bayes': GaussianNB(),
+    'Decision Trees': DecisionTreeClassifier(random_state=42),
+    'Random Forests': RandomForestClassifier(random_state=42),
+    'Support Vector Machines': SVC(random_state=42),
+    'K-Nearest Neighbors': KNeighborsClassifier()
+}
 
-score = accuracy_score(y_predict, y_test)
-print('{}% of samples were classified correctly !'.format(score * 100))
+#train and eval best model
+results = {}
+for name, model in classifiers.items():
+    model.fit(x_train, y_train)
+    y_predict = model.predict(x_test)
+    accuracy = accuracy_score(y_test, y_predict)
+    results[name] = accuracy
+    print(f'{name} Accuracy: {accuracy:.4f}')
+
+best_model = max(results, key=results.get)
+print(f'\nBest Model: {best_model} with Accuracy: {results[best_model]:.4f}')
